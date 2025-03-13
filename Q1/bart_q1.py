f349040bf1f0bfe48e1fc35b12ff77e98438db3c
Bart <bart.zwanenburg010@gmail.com>
2025-03-13 14:15:47 +0100

    Assignment Bart

---
+++ Q1/bart_q1.py
@@ -0,0 +1,15 @@
+
+num1 = 15
+num2 = 5
+
+addition_result = num1 + num2
+print(f"Addition result: {num1} + {num2} = {addition_result}")
+
+subtraction_result = num1 - num2
+print(f"Subtraction result: {num1} - {num2} = {subtraction_result}")
+
+divsion_result = num1 / num2
+print(f"Division result: {num1} / {num2} = {divsion_result}")
+
+multiplication_result = num1 * num2
+print(f"Multiplication result: {num1} * {num2} = {multiplication_result}")
